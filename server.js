076334db3e61364ebd845c3b78a88125be682ab1
Nima Khatamsazan <akhatamsazan@myseneca.ca>
2022-05-11 14:06:46 -0400

    section20

--- server.js
+++ server.js
@@ -3,17 +3,13 @@ const path = require("path");
 
 var app = express();
 
-app.listen(8080, function() {
-    console.log('Listening on port 8080');
+app.listen(5500, function() {
+    console.log('Listening on port 5500');
 });
 app.use(express.static("static"));
 
 //Change the './' to point to the root of your angular app
 app.get("/", (req, res) => {
-    res.sendFile(path.join(__dirname, "/main.html"));
+    res.sendFile(path.join(__dirname, "/index.html"));
 });
 
-//Send everything to your index.html page
-// app.get('/*', function(req, res) {
-//   res.sendFile(path.resolve('./main.html'));
-//  });
